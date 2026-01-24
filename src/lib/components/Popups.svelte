799d2fa4814da01bec69397baad1448b1cf81ab9
Harry <code1n1@pm.me>
2026-01-24 05:45:19 +0000

    mobile scroll behaviour

--- src/lib/components/Popups.svelte
+++ src/lib/components/Popups.svelte
@@ -23,7 +23,7 @@
         flex-direction: column;
         align-items: center;
         justify-content: flex-start;
-
+        
         padding-top: calc(var(--space-3) + var(--nav-height));
         gap: calc(var(--space-2) + var(--space-1));
 
